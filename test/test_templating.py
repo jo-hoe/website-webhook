ac1b3879b849bd26aa60c47980016c98e694813f
johannes <>
2025-12-29 15:04:26 +0100

    removing unneeded files

--- test/test_templating.py
+++
@@ -1,9 +0,0 @@
-
-from app.templating import template
-
-
-def test_templating_positive():
-    assert template("key", "this is a <<key>>", "test") == "this is a test"
-
-def test_templating_with_whitespaces():
-    assert template("key", "this is a << key >>", "test") == "this is a test"
